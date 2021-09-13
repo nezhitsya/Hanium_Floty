27dcda31611500457761021ec44f1408cce99746
ji-2 <cjh9180@naver.com>
2021-09-14 01:02:07 +0900

    search test commit

--- app/src/main/java/com/hanium/floty/SearchActivity.java
+++ app/src/main/java/com/hanium/floty/SearchActivity.java
@@ -1,115 +1,188 @@
 package com.hanium.floty;
 
-import android.content.res.AssetManager;
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserFactory;
 
-import java.io.BufferedReader;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.StringReader;
-import java.util.Vector;
+import java.net.URL;
+import java.net.URLEncoder;
 
 public class SearchActivity extends AppCompatActivity {
+
+    EditText edit;
+    TextView text;
+
+    String key="7g8DMDPXcjtzoLwfDH0delE%2B5LC6oQ9Q0NZT3O2STlgzydgE0BftsVOXt3TumvVVzbG0P8Y1ShlsCfsPkjweRw%3D%3D";
+
+    String data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Vector<plant> vPlant = new Vector<plant>();
+        edit= (EditText)findViewById(R.id.edit);
+        text= (TextView)findViewById(R.id.text);
+    }
 
-        String loadedXmlString = loadXML();
+
+    public void mOnClick(View v){
+
+        switch( v.getId() ){
+            case R.id.button:
+
+                new Thread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        data= getXmlData();
+
+
+                        runOnUiThread(new Runnable() {
+
+                            @Override
+                            public void run() {
+                                // TODO Auto-generated method stub
+                                text.setText(data);
+                            }
+                        });
+
+                    }
+                }).start();
+
+                break;
+        }
+
+    }
+    String getXmlData(){
+
+        StringBuffer buffer=new StringBuffer();
+
+        String str= edit.getText().toString();
+        String search = URLEncoder.encode(str);
+
+
+        String queryUrl="http://openapi.nature.go.kr/openapi/service/rest/PlantService/plntIlstrSearch?serviceKey="//요청 URL
+                +key+"&st=1&sw="+ search
+                +"&numOfRows=10&pageNo=1";
 
 
         try {
-            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
-            XmlPullParser parser = factory.newPullParser();
-            parser.setInput(new StringReader(loadedXmlString));
+            URL url= new URL(queryUrl);
+            InputStream is= url.openStream();
 
-            int eventType = parser.getEventType();
-            String tagName = "";
-            plant p = null;
+            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
+            XmlPullParser xpp= factory.newPullParser();
+            xpp.setInput( new InputStreamReader(is, "UTF-8") );
 
-            while(eventType != XmlPullParser.END_DOCUMENT){
+            String tag;
 
-                switch (eventType){
-                    case XmlPullParser.START_TAG:
-                        tagName = parser.getName();
-                        if(parser.getName().equals("plant")){
-                            p = new plant();
-                        }
+            xpp.next();
+            int eventType= xpp.getEventType();
+
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+                switch( eventType ){
+                    case XmlPullParser.START_DOCUMENT:
+                        buffer.append("시작\n\n");
                         break;
 
-                    case XmlPullParser.END_TAG:
-                        if(parser.getName().equals("plant")){
-                            vPlant.add(p);
-                            p=null;
+                    case XmlPullParser.START_TAG:
+                        tag= xpp.getName();
+
+                        if(tag.equals("item")) ;
+                        else if(tag.equals("familyKorNm")){
+                            buffer.append("과국명 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("familyNm")){
+                            buffer.append("과명 : ");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("genusKorNm")){
+                            buffer.append("속국명  :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("genusNm")){
+                            buffer.append("속명  :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("imgUrl")){
+                            buffer.append("이미지url :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("lastUpdtDtm")){
+                            buffer.append("최종수정일시 :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("  ,  ");
+                        }
+                        else if(tag.equals("+notRcmmGnrlNm")){
+                            buffer.append("비추천명  :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("plantGnrlNm")){
+                            buffer.append("국명  :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("plantPilbkNo")){
+                            buffer.append("도감번호  :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
+                        }
+                        else if(tag.equals("+plantSpecsScnm ")){
+                            buffer.append("정명학명  :");
+                            xpp.next();
+                            buffer.append(xpp.getText());
+                            buffer.append("\n");
                         }
                         break;
 
                     case XmlPullParser.TEXT:
-                        switch (tagName){
-                            case "plantkor":
-                                p.setPlantkor(parser.getText());
-                                break;
-                            case "planteng":
-                                p.setPlanteng(parser.getText());
-                                break;
-                            case "temp":
-                                p.setTemp(parser.getText());
-                                break;
-                            case "hum":
-                                p.setHum(parser.getText());
-                                break;
-                            case "imgurl":
-                                p.setImgurl(parser.getText());
-                                break;
-                        }
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        tag= xpp.getName();
+
+                        if(tag.equals("item")) buffer.append("\n");
+
                         break;
                 }
 
-                eventType = parser.next();
+                eventType= xpp.next();
             }
 
-        }catch (Exception e){
-
-
+        } catch (Exception e) {
+            // TODO Auto-generated catch blocke.printStackTrace();
         }
 
+        buffer.append("끝\n");
 
-    }
+        return buffer.toString();
 
-    // load xml string to string
-    private String loadXML() {
-        AssetManager assetManager = getResources().getAssets();
-        AssetManager.AssetInputStream ais = null;
-
-        try{
-            ais = (AssetManager.AssetInputStream)assetManager.open("plant.xml");
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(ais));
-
-        String line;
-        StringBuilder sb = new StringBuilder();
-
-        try{
-            while((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return sb.toString();
     }
 }
