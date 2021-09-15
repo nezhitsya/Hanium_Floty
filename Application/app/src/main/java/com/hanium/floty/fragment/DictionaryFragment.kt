8f6897cd1df2e232e642b15ef4fa3d02083e65e0
nezhitsya <huyn9295@naver.com>
2021-09-15 17:41:00 +0900

    set up dictionary

--- Application/app/src/main/java/com/hanium/floty/fragment/DictionaryFragment.kt
+++ Application/app/src/main/java/com/hanium/floty/fragment/DictionaryFragment.kt
@@ -1,6 +1,8 @@
 package com.hanium.floty.fragment
 
+import android.content.res.AssetManager
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +10,22 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.hanium.floty.R
+import com.hanium.floty.adapter.DiaryListAdapter
+import com.hanium.floty.adapter.DictionaryAdapter
+import com.hanium.floty.model.Diary
+import com.hanium.floty.model.Plant
+import org.xmlpull.v1.XmlPullParser
+import org.xmlpull.v1.XmlPullParserException
+import org.xmlpull.v1.XmlPullParserFactory
+import java.io.IOException
+import java.io.InputStream
+import java.lang.Exception
 
 class DictionaryFragment : Fragment() {
 
     private lateinit var recyclerView: RecyclerView
+    lateinit var pullParserFactory: XmlPullParserFactory
+    var plantList = arrayListOf<Plant>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -38,10 +52,39 @@ class DictionaryFragment : Fragment() {
         recyclerView = view.findViewById(R.id.recycler_view)
         recyclerView.setHasFixedSize(true)
 
+        try {
+            var xpp: XmlPullParser = resources.getXml(R.xml.plant)
+
+            while (xpp.eventType != XmlPullParser.END_DOCUMENT) {
+                if (xpp.eventType == XmlPullParser.START_TAG) {
+                    if (xpp.name.equals("plant")) {
+                        val plant = Plant()
+                        plant.plantkor = xpp.getAttributeValue(3)
+                        plant.planteng = xpp.getAttributeValue(2)
+                        plant.temp = xpp.getAttributeValue(4)
+                        plant.hum = xpp.getAttributeValue(0)
+                        plant.imgurl = xpp.getAttributeValue(1)
+
+                        plantList.add(plant)
+                    }
+                }
+                xpp.next()
+            }
+            Log.e("MY_VALUE", plantList.toString())
+        } catch (e: XmlPullParserException) {
+            e.printStackTrace()
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+
         var linearLayoutManager = LinearLayoutManager(context)
         linearLayoutManager.reverseLayout = true
         linearLayoutManager.stackFromEnd = true
         recyclerView.layoutManager = linearLayoutManager
+
+        val adapter = DictionaryAdapter(context!!, plantList)
+        recyclerView?.adapter = adapter
+        adapter.notifyDataSetChanged()
 
         return view
     }
---
+++ Application/app/src/main/java/com/hanium/floty/model/Plant.java
@@ -0,0 +1,51 @@
+package com.hanium.floty.model;
+
+public class Plant {
+
+    String plantkor;
+    String planteng;
+    String temp;
+    String hum;
+    String imgurl;
+
+    public String getPlantkor() {
+        return plantkor;
+    }
+
+    public void setPlantkor(String plantkor) {
+        this.plantkor = plantkor;
+    }
+
+    public String getPlanteng() {
+        return planteng;
+    }
+
+    public void setPlanteng(String planteng) {
+        this.planteng = planteng;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setTemp(String temp) {
+        this.temp = temp;
+    }
+
+    public String getHum() {
+        return hum;
+    }
+
+    public void setHum(String hum) {
+        this.hum = hum;
+    }
+
+    public String getImgurl() {
+        return imgurl;
+    }
+
+    public void setImgurl(String imgurl) {
+        this.imgurl = imgurl;
+    }
+
+}
